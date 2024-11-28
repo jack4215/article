986a3fbd833c147f3cc4c4fe2f55bfaa99efb420
Jack Lu <87149267+jack4215@users.noreply.github.com>
2024-11-29 01:12:07 +0800

    update

---
+++ js/time.js
@@ -0,0 +1,26 @@
+function startTime(){   
+    var today = new Date();
+    today.setHours(today.getHours() + 8);
+    
+    var yyyy = today.getUTCFullYear();
+    var MM = today.getUTCMonth() + 1;
+    var dd = today.getUTCDate();
+    var hh = today.getUTCHours();
+    var mm = today.getUTCMinutes();
+    var ss = today.getUTCSeconds();
+    MM = checkTime(MM);
+    dd = checkTime(dd);
+    hh = checkTime(hh);
+    mm = checkTime(mm);
+    ss = checkTime(ss);    
+  
+    document.getElementById('nowDateTimeSpan').innerHTML=yyyy+"-"+MM +"-"+ dd +" " + hh+":"+mm+":"+ss;   
+    setTimeout('startTime()',1000);
+  }   
+  
+  function checkTime(i){   
+      if (i < 10){
+          i = "0" + i;
+      }   
+        return i;
+  }  
